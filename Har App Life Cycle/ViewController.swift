c5259b5715dd8b06476fb6287a9caa76dec835ba
Haryanto Quang <quang.haryanto@gmail.com>
2021-05-19 14:55:51 +0700

    Initial Commit

---
+++ Har App Life Cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Har App Life Cycle
+//
+//  Created by Haryanto Salim on 19/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
